9211aba4fdb2153eb2ee9224761784fc59ffeb87
Michael Hoppes <mhoppes@i3logix.com>
2019-05-22 09:27:09 -0600

    leap

---
+++ leap/leap.js
@@ -0,0 +1,9 @@
+export const isLeap = (year) => {
+    if (year % 4 !== 0) {
+        return false;
+    }
+    if (year % 100 !== 0) {
+        return true;
+    }
+    return year % 400 === 0;
+};
